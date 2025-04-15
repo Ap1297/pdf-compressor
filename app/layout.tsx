017135a6049da1a42341f6722136854764e2f266
Ankit Panchal <ankitp@voizzit.com>
2025-04-15 10:54:08 +0400

    Title changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'ThunderBolt Pdf Compressor',
+  description: 'Created with nextjs and springboot',
   generator: 'v0.dev',
 }
 
